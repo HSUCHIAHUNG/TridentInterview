0a139a780e57e460f41bd3ea962126c06b7117e6
HSUCHIAHUNG <h0989541162@gmail.com>
2024-03-06 16:55:11 +0800

    壓縮圖檔

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -3,17 +3,17 @@
 </script>
 
 <template>
-  <div class="bg-#DCCCBC w-full h-full px-10% md:col-span-2">
+  <div class="bg-#DCCCBC w-full h-full px-10% md:px-5% md:col-span-2">
     <ul class="flex flex-wrap justify-between gap-50px py-50px">
-      <li class="w-300px flex justify-between">
+      <li class="w-278px flex justify-between">
         <p class="relative text-36px font-bold pr-25px after-absolute after-content-[''] after-border-8px after-w-25px after-h-25px after-border-#AA666699/60 after-rounded-50% after-left-28px after-top-85px ">外觀</p>
         <p>白頭鵯體長約17到22公分，額至頭頂純黑色而富有光澤，兩眼上方至後枕白色，形成一白色枕環。耳羽後部有一白斑，此白環與白斑在黑色的頭部均極為醒目，老鳥的枕羽(後頭部)更潔白，所以又叫「白頭翁」。</p>
       </li>
-      <li class="w-300px flex">
+      <li class="w-278px flex">
         <p class="relative text-36px font-bold pr-25px after-absolute after-content-[''] after-border-8px after-w-25px after-h-25px after-border-#AA666699/60 after-rounded-50% after-left-28px after-top-85px ">棲地</p>
         <p>白頭翁和麻雀、綠繡眼合稱「城市三寶」，常成群出現在平原區灌木叢、丘陵樹林地帶，以及校園、公園、庭院、行道中的各種高高的電線與樹上。</p>
       </li>
-      <li class="w-300px flex ">
+      <li class="w-278px flex ">
         <p class="relative text-36px font-bold pr-25px after-absolute after-content-[''] after-border-8px after-w-25px after-h-25px after-border-#AA666699/60 after-rounded-50% after-left-28px after-top-85px ">食性</p>
         <p>以果樹的漿果和種子為主食，並時常飛入果園偷吃果實，還會吃嫩葉嫩芽，尤其是胡蝶蘭的嫩葉嫩芽葉，偶爾啄食昆蟲。</p>
       </li>
--- src/views/story.vue
+++ src/views/story.vue
@@ -2,7 +2,7 @@
   <div class=" min-h-[calc(100vh-580px)] row-span-2 w-full bg-[url(public/images/bird1.jpg)] bg-no-repeat bg-cover bg-center
     sm:min-h-[calc(100vh-480px)] md:min-h-[calc(100vh-350px)] xl:min-h-[calc(100vh-250px)] md:col-span-2
   ">
-    <div class="w-90% m-[0_auto] text-right pt-63px md:h-full md:flex md:flex-col md:justify-end md:pb-36px">
+    <div class="w-90% m-[0_auto] text-right pt-63px pb-24px md:h-full md:flex md:flex-col md:justify-end md:pb-36px">
       <p class="text-48px text-white font-bold text-shadow-lg">白頭翁 (Chinese bulbul)</p>
       <p class="text-white text-shadow-xl">又名白頭鵯。以果實、昆蟲為主食，無法消化小米、穀類。平均壽命約 8~10 年。</p>
     </div>
