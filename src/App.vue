77d83fc07a2dde4ad03de6b390db4e918c73cce9
HSUCHIAHUNG <h0989541162@gmail.com>
2024-03-06 16:15:32 +0800

    刪除不必要的資料

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,11 @@ import Footer from '@/components/layouts/Footer.vue';
 </script>
 
 <template>
-  <!-- <div class="flex flex-col min-h-100vh"> -->
   <div class=" grid grid-row-[auto_1fr_auto] m-0 md:grid-cols-[345px_auto_auto] ">
     <Header/>
     <router-view v-slot="{ Component, route }">
         <transition name="fade" mode="out-in">
-            <!-- <div :key="route.name" class=" flex-1 w-full bg-[url(public/images/banner.png)] bg-no-repeat bg-cover bg-center "> -->
-              <component :key="route.name" :is="Component"></component>
-            <!-- </div> -->
+            <component :key="route.name" :is="Component"></component>
         </transition>
       </router-view>
     <Footer/>
--- src/layout/DefaultLayout.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-  <div class=" min-h-100vh w-full bg-[url(public/images/banner.png)] bg-no-repeat bg-cover bg-center">
-    <router-view/>
-  </div>
-</template>
